fc7be3617868d29f189151382cc5da94bcea2075
Dmytro Golub <dmytro.golub@paysafe.com>
2018-03-28 18:01:51 +0100

    Initial Commit

---
+++ ImageSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageSearch
+//
+//  Created by Dmytro Golub on 28/03/2018.
+//  Copyright © 2018 Paysafe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
